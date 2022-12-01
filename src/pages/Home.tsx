c2fcb3a3ef535c4de44a7c2a25b64a312df55873
sahanenes <sahanenesss@gmail.com>
2022-12-01 14:56:58 +0300

    getTodos added (data)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,33 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react'
-import InputForm from '../components/InputForm'
-import TodoList from '../components/TodoList';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import InputForm from "../components/InputForm";
+import TodoList from "../components/TodoList";
 
+const url = "https://6351820f3e9fa1244e6084e3.mockapi.io/api/todos";
 
-const Home = () => {
-  
-  
-
-  return (
-    <div className='main'>
-      <InputForm  />
-      <TodoList />
-    </div>
-  )
+interface TodoType {
+  id: string | number;
+  task: string;
+  isDone: boolean;
 }
 
-export default Home
+const Home = () => {
+  const [todos, setTodos] = useState<TodoType[]>([]);
+
+  const getTodos = async () => {
+    const { data } = await axios.get(url);
+    console.log(data);
+  };
+  useEffect(() => {
+    getTodos();
+  }, []);
+
+  return (
+    <div className="main">
+      <InputForm />
+      <TodoList />
+    </div>
+  );
+};
+
+export default Home;
