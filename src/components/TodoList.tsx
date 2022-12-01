e021dc2e4047085f5a03ca9935ca6205faaffc0e
sahanenes <sahanenesss@gmail.com>
2022-12-01 15:27:13 +0300

    todolistitem added

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -8,7 +8,9 @@ interface ITodoList {
 const TodoList: React.FC<ITodoList> = ({ todos }) => {
   return (
     <ul>
-      <TodoListItem />
+      {todos.map((todo) => (
+        <TodoListItem key={todo.id} todo={todo} />
+      ))}
     </ul>
   );
 };
--- src/components/TodoListItem.tsx
+++ src/components/TodoListItem.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
-const TodoListItem = () => {
+import React from "react";
+
+interface ITodoListItem {
+  todo: TodoType;
+}
+const TodoListItem: React.FC<ITodoListItem> = ({ todo }) => {
   return (
     <li>
-      <p className='checked'>Todo </p>
+      <p className="checked">Todo </p>
       <p> Todo </p>
-      <span className="task-icons" >✖️</span>
+      <span className="task-icons">✖️</span>
     </li>
   );
 };
 
-export default TodoListItem
+export default TodoListItem;
